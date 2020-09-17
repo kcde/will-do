09a47edf6ceb3420d76f12702cac641fc437e31f
keside <wilsonezeala@yahoo.com>
2020-09-17 02:57:38 +0100

    new task add functionality almost complete

--- app/app.js
+++ app/app.js
@@ -2,104 +2,114 @@ const listDate = document.querySelector(".list-date");
 const taskAmount = document.querySelector(".task-amount");
 const taskContainer = document.querySelector(".tasks");
 const tasks = document.querySelectorAll(".task");
-
 const newTaskBtn = document.querySelector(".add-btn");
 const newTask = document.querySelector(".new-task");
 const date = new Date();
 
-// getting the name of the present month
-const getMonthName = () => {
-    let months = [
-        "january",
-        "february",
-        "march",
-        "april",
-        "may",
-        "june",
-        "july",
-        "august",
-        "september",
-        "october",
-        "november",
-        "december",
-    ];
-    const currentMonth = months[date.getMonth()];
-    return currentMonth.toString();
+const fullDateRender = () => {
+    // getting the name of the present month
+    const getMonthName = () => {
+        let months = [
+            "january",
+            "february",
+            "march",
+            "april",
+            "may",
+            "june",
+            "july",
+            "august",
+            "september",
+            "october",
+            "november",
+            "december",
+        ];
+        const currentMonth = months[date.getMonth()];
+        return currentMonth.toString();
+    };
+
+    // getting the name of the present day
+    const getDayName = () => {
+        const days = [
+            "sunday",
+            "tuesday",
+            "wednesday",
+            "thursday",
+            "friday",
+            "saturday",
+            "sunday",
+        ];
+        const currentDay = days[date.getDay()];
+        return currentDay.split("").slice(0, 3).join("");
+    };
+
+    // full date to display on webpage
+    const fullDate =
+        getDayName() +
+        ", " +
+        getMonthName() +
+        " " +
+        date.getDate() +
+        ", " +
+        date.getFullYear();
+
+    // rendering date on web page
+    listDate.innerHTML = fullDate;
 };
+function render() {
+    if (localStorage.getItem("list")) {
+        taskContainer.innerHTML = localStorage.getItem("list");
+    }
+}
 
-// getting the name of the present day
-const getDayName = () => {
-    const days = [
-        "sunday",
-        "tuesday",
-        "wednesday",
-        "thursday",
-        "friday",
-        "saturday",
-        "sunday",
-    ];
-    const currentDay = days[date.getDay()];
-    return currentDay.split("").slice(0, 3).join("");
-};
+function save() {
+    localStorage.setItem("list", taskContainer.innerHTML);
+}
 
-// full date to display on webpage
-const fullDate =
-    getDayName() +
-    ", " +
-    getMonthName() +
-    " " +
-    date.getDate() +
-    ", " +
-    date.getFullYear();
+function taskCounter() {
+    taskCount++;
+}
 
-// rendering date on web page
-listDate.innerHTML = fullDate;
+function addTask(task) {
+    const icon = document.createElement("i");
+    icon.classList.add("fa");
+    icon.classList.add("fa-check");
+    icon.classList.add("done-icon");
 
-// list of tasks
-const taskList = [{ name: "blessing" }, { name: "kels" }];
+    const inner = document.createElement("div");
+    inner.classList.add("task-icon");
+    inner.appendChild(icon);
 
-//function to add task to task list array
-const addTask = (task) => {
-    id = taskList.length + 1;
-    taskList.push({ id: id, name: task, done: false });
-};
+    const text = document.createElement("p");
+    text.innerHTML = task;
 
-const taskIcons = document.querySelectorAll(".task-icon");
-taskIcons.forEach((icon) => {
-    icon.addEventListener("click", () => {
-        icon.parentElement.classList.add("done");
-    });
-});
+    const outer = document.createElement("div");
+    outer.classList.add("task");
+    outer.appendChild(text);
+    outer.appendChild(inner);
+
+    taskContainer.appendChild(outer);
+}
+
+fullDateRender();
+render();
 
 // click button to add task
 newTaskBtn.addEventListener("click", () => {
     if (newTaskBtn.classList.contains("add-btn-clicked")) {
         newTaskBtn.classList.remove("add-btn-clicked");
         newTask.classList.remove("new-task-show");
-        console.log(newTask.value);
-        console.log(taskList);
-
+        addTask(newTask.value);
         newTask.value = "";
+        save();
     } else {
         newTaskBtn.classList.add("add-btn-clicked");
         newTask.classList.add("new-task-show");
     }
 });
 
-//enter button to add task
-window.addEventListener("keyup", (e) => {
-    if (newTaskBtn.classList.contains("add-btn-clicked")) {
-        if (e.keyCode === 13) {
-            newTask.classList.remove("new-task-show");
-            newTaskBtn.classList.remove("add-btn-clicked");
-            newTask.classList.remove("new-task-show");
-            console.log(newTask.value);
-            console.log(taskList);
-            newTask.value = "";
-        }
-    } else {
-        return;
+taskContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("task-icon")) {
+        e.target.parentElement.classList.add("done");
+        save();
     }
 });
-
-// localStorage.setItem("name", JSON.stringify({ loud: "gim" }));
